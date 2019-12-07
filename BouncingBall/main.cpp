bcaf14f9538d989020cfee953b18d15195a3863f
Đỗ Tường Hoàng Phú <55187582+dophu1402@users.noreply.github.com>
2019-12-07 22:40:28 +0700

    Update main.cpp

--- BouncingBall/main.cpp
+++ BouncingBall/main.cpp
@@ -1,9 +1,10 @@
 #include <SFML/Graphics.hpp>
 #include <Windows.h>
-#include <iostream>
 #include "design.h"
 #include "designGame.h"
 #include "game.h"
+#include "Header.h"
+#include <iostream>
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
@@ -17,11 +18,16 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	newgame->setSize(200, 50);
 	newgame->setPosition(150, 170);
 
+	tgui::Button::Ptr arcade = tgui::Button::create("Arcade Mode");
+	arcade->setSize(200, 50);
+	arcade->setPosition(150, 240);
+
 	tgui::Button::Ptr quitgame = tgui::Button::create("Quit Game");
 	quitgame->setSize(200, 50);
-	quitgame->setPosition(150, 240);
+	quitgame->setPosition(150, 310);
 
 	gui.add(newgame);
+	gui.add(arcade);
 	gui.add(quitgame);
 
 	
@@ -53,9 +59,55 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 		}
 
 	});
+
+	arcade->connect("pressed", [&]() {
+		arcadeMode();
+		/*sf::RenderWindow ArcadeWindow(sf::VideoMode(550, 750), "Arcade Mode", sf::Style::Close);
+		ArcadeWindow.setFramerateLimit(60);
+		Ball ball(10);
+		brick a,c,d;
+		std::vector<brick> br;
+		for (int i = 0; i < 20; i++)
+		{
+			br.push_back(a);
+		}
+		c.setPossition({ 10,30 });
+		d.setPossition({ 500,30 });
+		generateBrick(br);
+
+		while (ArcadeWindow.isOpen())
+		{
+			sf::Event event;
+			while (ArcadeWindow.pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed)
+				{
+					ArcadeWindow.close();
+				}
+			}
+
+			for (int i = 0; i < br.size(); i++)
+			{
+				if (br[i].isHitBall(ball))
+				{
+					br[i].setSize({ 0,0 });
+				}
+			}
+			ball.move({ 0, 5 });
+			
+			ArcadeWindow.clear();
+			ball.drawTo(ArcadeWindow);
+			for (int i = 0; i < br.size(); i++) {
+				br[i].drawTo(ArcadeWindow);
+			}
+
+			ArcadeWindow.display();
+
+		}*/
+	});
 	
 	quitgame->connect("pressed", []() {
-		
+		exit(0);
 	});
 
 	bool checkChooseWindow = true;
@@ -82,5 +134,4 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	}
 
 	return EXIT_SUCCESS;
-
 }
